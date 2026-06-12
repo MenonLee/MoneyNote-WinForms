2373847cc968477b032fd84eb5dd206c21a7e5a8
MenonLee <rkong03@naver.com>
2026-06-13 01:50:36 +0900

    [Fix] Compact budget management window

--- ScheduleProject/ScheduleProject/Forms/FormBudget.Designer.cs
+++ ScheduleProject/ScheduleProject/Forms/FormBudget.Designer.cs
@@ -60,7 +60,7 @@ namespace ScheduleProject
             lblTitle.AutoSize = true;
             lblTitle.Font = new Font("맑은 고딕", 24F, FontStyle.Bold, GraphicsUnit.Point, 129);
             lblTitle.ForeColor = Color.FromArgb(17, 24, 39);
-            lblTitle.Location = new Point(40, 30);
+            lblTitle.Location = new Point(32, 24);
             lblTitle.Name = "lblTitle";
             lblTitle.Size = new Size(165, 45);
             lblTitle.TabIndex = 0;
@@ -71,7 +71,7 @@ namespace ScheduleProject
             lblSubtitle.AutoSize = true;
             lblSubtitle.Font = new Font("맑은 고딕", 10F, FontStyle.Regular, GraphicsUnit.Point, 129);
             lblSubtitle.ForeColor = Color.FromArgb(100, 116, 139);
-            lblSubtitle.Location = new Point(44, 73);
+            lblSubtitle.Location = new Point(36, 67);
             lblSubtitle.Name = "lblSubtitle";
             lblSubtitle.Size = new Size(332, 19);
             lblSubtitle.TabIndex = 1;
@@ -83,9 +83,9 @@ namespace ScheduleProject
             panelMonth.BorderStyle = BorderStyle.FixedSingle;
             panelMonth.Controls.Add(dateBudgetMonth);
             panelMonth.Controls.Add(lblMonth);
-            panelMonth.Location = new Point(590, 40);
+            panelMonth.Location = new Point(476, 34);
             panelMonth.Name = "panelMonth";
-            panelMonth.Size = new Size(270, 56);
+            panelMonth.Size = new Size(250, 52);
             panelMonth.TabIndex = 2;
             // 
             // dateBudgetMonth
@@ -93,7 +93,7 @@ namespace ScheduleProject
             dateBudgetMonth.CustomFormat = "yyyy년 MM월";
             dateBudgetMonth.Font = new Font("맑은 고딕", 10F, FontStyle.Regular, GraphicsUnit.Point, 129);
             dateBudgetMonth.Format = DateTimePickerFormat.Custom;
-            dateBudgetMonth.Location = new Point(96, 15);
+            dateBudgetMonth.Location = new Point(86, 13);
             dateBudgetMonth.Name = "dateBudgetMonth";
             dateBudgetMonth.ShowUpDown = true;
             dateBudgetMonth.Size = new Size(150, 25);
@@ -105,7 +105,7 @@ namespace ScheduleProject
             lblMonth.AutoSize = true;
             lblMonth.Font = new Font("맑은 고딕", 10F, FontStyle.Bold, GraphicsUnit.Point, 129);
             lblMonth.ForeColor = Color.FromArgb(30, 41, 59);
-            lblMonth.Location = new Point(24, 18);
+            lblMonth.Location = new Point(22, 16);
             lblMonth.Name = "lblMonth";
             lblMonth.Size = new Size(51, 19);
             lblMonth.TabIndex = 0;
@@ -119,9 +119,9 @@ namespace ScheduleProject
             panelMonthly.Controls.Add(textMonthlyBudget);
             panelMonthly.Controls.Add(lblMonthlyGuide);
             panelMonthly.Controls.Add(lblMonthlyTitle);
-            panelMonthly.Location = new Point(40, 120);
+            panelMonthly.Location = new Point(32, 105);
             panelMonthly.Name = "panelMonthly";
-            panelMonthly.Size = new Size(395, 145);
+            panelMonthly.Size = new Size(340, 132);
             panelMonthly.TabIndex = 3;
             // 
             // buttonSaveMonthly
@@ -131,7 +131,7 @@ namespace ScheduleProject
             buttonSaveMonthly.FlatStyle = FlatStyle.Flat;
             buttonSaveMonthly.Font = new Font("맑은 고딕", 10F, FontStyle.Bold, GraphicsUnit.Point, 129);
             buttonSaveMonthly.ForeColor = Color.White;
-            buttonSaveMonthly.Location = new Point(268, 91);
+            buttonSaveMonthly.Location = new Point(236, 84);
             buttonSaveMonthly.Name = "buttonSaveMonthly";
             buttonSaveMonthly.Size = new Size(94, 34);
             buttonSaveMonthly.TabIndex = 3;
@@ -143,10 +143,10 @@ namespace ScheduleProject
             // 
             textMonthlyBudget.BorderStyle = BorderStyle.FixedSingle;
             textMonthlyBudget.Font = new Font("맑은 고딕", 11F, FontStyle.Regular, GraphicsUnit.Point, 129);
-            textMonthlyBudget.Location = new Point(28, 94);
+            textMonthlyBudget.Location = new Point(24, 87);
             textMonthlyBudget.Name = "textMonthlyBudget";
             textMonthlyBudget.PlaceholderText = "예: 500000";
-            textMonthlyBudget.Size = new Size(224, 27);
+            textMonthlyBudget.Size = new Size(200, 27);
             textMonthlyBudget.TabIndex = 2;
             // 
             // lblMonthlyGuide
@@ -154,7 +154,7 @@ namespace ScheduleProject
             lblMonthlyGuide.AutoSize = true;
             lblMonthlyGuide.Font = new Font("맑은 고딕", 9.5F, FontStyle.Regular, GraphicsUnit.Point, 129);
             lblMonthlyGuide.ForeColor = Color.FromArgb(100, 116, 139);
-            lblMonthlyGuide.Location = new Point(28, 54);
+            lblMonthlyGuide.Location = new Point(24, 52);
             lblMonthlyGuide.Name = "lblMonthlyGuide";
             lblMonthlyGuide.Size = new Size(259, 17);
             lblMonthlyGuide.TabIndex = 1;
@@ -165,7 +165,7 @@ namespace ScheduleProject
             lblMonthlyTitle.AutoSize = true;
             lblMonthlyTitle.Font = new Font("맑은 고딕", 13F, FontStyle.Bold, GraphicsUnit.Point, 129);
             lblMonthlyTitle.ForeColor = Color.FromArgb(15, 23, 42);
-            lblMonthlyTitle.Location = new Point(26, 24);
+            lblMonthlyTitle.Location = new Point(24, 20);
             lblMonthlyTitle.Name = "lblMonthlyTitle";
             lblMonthlyTitle.Size = new Size(126, 25);
             lblMonthlyTitle.TabIndex = 0;
@@ -181,9 +181,9 @@ namespace ScheduleProject
             panelCategory.Controls.Add(lblCategoryAmount);
             panelCategory.Controls.Add(lblCategoryName);
             panelCategory.Controls.Add(lblCategoryTitle);
-            panelCategory.Location = new Point(465, 120);
+            panelCategory.Location = new Point(386, 105);
             panelCategory.Name = "panelCategory";
-            panelCategory.Size = new Size(395, 145);
+            panelCategory.Size = new Size(340, 132);
             panelCategory.TabIndex = 4;
             // 
             // buttonSaveCategory
@@ -193,7 +193,7 @@ namespace ScheduleProject
             buttonSaveCategory.FlatStyle = FlatStyle.Flat;
             buttonSaveCategory.Font = new Font("맑은 고딕", 10F, FontStyle.Bold, GraphicsUnit.Point, 129);
             buttonSaveCategory.ForeColor = Color.White;
-            buttonSaveCategory.Location = new Point(270, 91);
+            buttonSaveCategory.Location = new Point(222, 84);
             buttonSaveCategory.Name = "buttonSaveCategory";
             buttonSaveCategory.Size = new Size(94, 34);
             buttonSaveCategory.TabIndex = 5;
@@ -205,10 +205,10 @@ namespace ScheduleProject
             // 
             textCategoryBudget.BorderStyle = BorderStyle.FixedSingle;
             textCategoryBudget.Font = new Font("맑은 고딕", 10F, FontStyle.Regular, GraphicsUnit.Point, 129);
-            textCategoryBudget.Location = new Point(141, 96);
+            textCategoryBudget.Location = new Point(126, 89);
             textCategoryBudget.Name = "textCategoryBudget";
             textCategoryBudget.PlaceholderText = "예: 120000";
-            textCategoryBudget.Size = new Size(113, 25);
+            textCategoryBudget.Size = new Size(86, 25);
             textCategoryBudget.TabIndex = 4;
             // 
             // comboCategory
@@ -216,7 +216,7 @@ namespace ScheduleProject
             comboCategory.DropDownStyle = ComboBoxStyle.DropDownList;
             comboCategory.Font = new Font("맑은 고딕", 10F, FontStyle.Regular, GraphicsUnit.Point, 129);
             comboCategory.FormattingEnabled = true;
-            comboCategory.Location = new Point(28, 96);
+            comboCategory.Location = new Point(24, 89);
             comboCategory.Name = "comboCategory";
             comboCategory.Size = new Size(97, 25);
             comboCategory.TabIndex = 3;
@@ -226,7 +226,7 @@ namespace ScheduleProject
             lblCategoryAmount.AutoSize = true;
             lblCategoryAmount.Font = new Font("맑은 고딕", 9.5F, FontStyle.Bold, GraphicsUnit.Point, 129);
             lblCategoryAmount.ForeColor = Color.FromArgb(30, 41, 59);
-            lblCategoryAmount.Location = new Point(141, 74);
+            lblCategoryAmount.Location = new Point(126, 68);
             lblCategoryAmount.Name = "lblCategoryAmount";
             lblCategoryAmount.Size = new Size(34, 17);
             lblCategoryAmount.TabIndex = 2;
@@ -237,7 +237,7 @@ namespace ScheduleProject
             lblCategoryName.AutoSize = true;
             lblCategoryName.Font = new Font("맑은 고딕", 9.5F, FontStyle.Bold, GraphicsUnit.Point, 129);
             lblCategoryName.ForeColor = Color.FromArgb(30, 41, 59);
-            lblCategoryName.Location = new Point(28, 74);
+            lblCategoryName.Location = new Point(24, 68);
             lblCategoryName.Name = "lblCategoryName";
             lblCategoryName.Size = new Size(60, 17);
             lblCategoryName.TabIndex = 1;
@@ -248,7 +248,7 @@ namespace ScheduleProject
             lblCategoryTitle.AutoSize = true;
             lblCategoryTitle.Font = new Font("맑은 고딕", 13F, FontStyle.Bold, GraphicsUnit.Point, 129);
             lblCategoryTitle.ForeColor = Color.FromArgb(15, 23, 42);
-            lblCategoryTitle.Location = new Point(26, 24);
+            lblCategoryTitle.Location = new Point(24, 20);
             lblCategoryTitle.Name = "lblCategoryTitle";
             lblCategoryTitle.Size = new Size(155, 25);
             lblCategoryTitle.TabIndex = 0;
@@ -266,9 +266,9 @@ namespace ScheduleProject
             panelSummary.Controls.Add(lblExpenseSummary);
             panelSummary.Controls.Add(lblMonthSummaryValue);
             panelSummary.Controls.Add(lblMonthSummary);
-            panelSummary.Location = new Point(40, 292);
+            panelSummary.Location = new Point(32, 258);
             panelSummary.Name = "panelSummary";
-            panelSummary.Size = new Size(820, 92);
+            panelSummary.Size = new Size(694, 82);
             panelSummary.TabIndex = 5;
             // 
             // lblRemainSummaryValue
@@ -276,7 +276,7 @@ namespace ScheduleProject
             lblRemainSummaryValue.AutoSize = true;
             lblRemainSummaryValue.Font = new Font("맑은 고딕", 13F, FontStyle.Bold, GraphicsUnit.Point, 129);
             lblRemainSummaryValue.ForeColor = Color.FromArgb(15, 118, 110);
-            lblRemainSummaryValue.Location = new Point(632, 43);
+            lblRemainSummaryValue.Location = new Point(532, 39);
             lblRemainSummaryValue.Name = "lblRemainSummaryValue";
             lblRemainSummaryValue.Size = new Size(22, 25);
             lblRemainSummaryValue.TabIndex = 7;
@@ -287,7 +287,7 @@ namespace ScheduleProject
             lblRemainSummary.AutoSize = true;
             lblRemainSummary.Font = new Font("맑은 고딕", 9F, FontStyle.Regular, GraphicsUnit.Point, 129);
             lblRemainSummary.ForeColor = Color.FromArgb(100, 116, 139);
-            lblRemainSummary.Location = new Point(632, 22);
+            lblRemainSummary.Location = new Point(532, 19);
             lblRemainSummary.Name = "lblRemainSummary";
             lblRemainSummary.Size = new Size(59, 15);
             lblRemainSummary.TabIndex = 6;
@@ -298,7 +298,7 @@ namespace ScheduleProject
             lblCategorySummaryValue.AutoSize = true;
             lblCategorySummaryValue.Font = new Font("맑은 고딕", 13F, FontStyle.Bold, GraphicsUnit.Point, 129);
             lblCategorySummaryValue.ForeColor = Color.FromArgb(194, 65, 12);
-            lblCategorySummaryValue.Location = new Point(432, 43);
+            lblCategorySummaryValue.Location = new Point(360, 39);
             lblCategorySummaryValue.Name = "lblCategorySummaryValue";
             lblCategorySummaryValue.Size = new Size(22, 25);
             lblCategorySummaryValue.TabIndex = 5;
@@ -309,7 +309,7 @@ namespace ScheduleProject
             lblCategorySummary.AutoSize = true;
             lblCategorySummary.Font = new Font("맑은 고딕", 9F, FontStyle.Regular, GraphicsUnit.Point, 129);
             lblCategorySummary.ForeColor = Color.FromArgb(100, 116, 139);
-            lblCategorySummary.Location = new Point(432, 22);
+            lblCategorySummary.Location = new Point(360, 19);
             lblCategorySummary.Name = "lblCategorySummary";
             lblCategorySummary.Size = new Size(101, 15);
             lblCategorySummary.TabIndex = 4;
@@ -320,7 +320,7 @@ namespace ScheduleProject
             lblExpenseSummaryValue.AutoSize = true;
             lblExpenseSummaryValue.Font = new Font("맑은 고딕", 13F, FontStyle.Bold, GraphicsUnit.Point, 129);
             lblExpenseSummaryValue.ForeColor = Color.FromArgb(37, 99, 235);
-            lblExpenseSummaryValue.Location = new Point(232, 43);
+            lblExpenseSummaryValue.Location = new Point(190, 39);
             lblExpenseSummaryValue.Name = "lblExpenseSummaryValue";
             lblExpenseSummaryValue.Size = new Size(22, 25);
             lblExpenseSummaryValue.TabIndex = 3;
@@ -331,7 +331,7 @@ namespace ScheduleProject
             lblExpenseSummary.AutoSize = true;
             lblExpenseSummary.Font = new Font("맑은 고딕", 9F, FontStyle.Regular, GraphicsUnit.Point, 129);
             lblExpenseSummary.ForeColor = Color.FromArgb(100, 116, 139);
-            lblExpenseSummary.Location = new Point(232, 22);
+            lblExpenseSummary.Location = new Point(190, 19);
             lblExpenseSummary.Name = "lblExpenseSummary";
             lblExpenseSummary.Size = new Size(75, 15);
             lblExpenseSummary.TabIndex = 2;
@@ -342,7 +342,7 @@ namespace ScheduleProject
             lblMonthSummaryValue.AutoSize = true;
             lblMonthSummaryValue.Font = new Font("맑은 고딕", 13F, FontStyle.Bold, GraphicsUnit.Point, 129);
             lblMonthSummaryValue.ForeColor = Color.FromArgb(15, 23, 42);
-            lblMonthSummaryValue.Location = new Point(32, 43);
+            lblMonthSummaryValue.Location = new Point(28, 39);
             lblMonthSummaryValue.Name = "lblMonthSummaryValue";
             lblMonthSummaryValue.Size = new Size(22, 25);
             lblMonthSummaryValue.TabIndex = 1;
@@ -353,7 +353,7 @@ namespace ScheduleProject
             lblMonthSummary.AutoSize = true;
             lblMonthSummary.Font = new Font("맑은 고딕", 9F, FontStyle.Regular, GraphicsUnit.Point, 129);
             lblMonthSummary.ForeColor = Color.FromArgb(100, 116, 139);
-            lblMonthSummary.Location = new Point(32, 22);
+            lblMonthSummary.Location = new Point(28, 19);
             lblMonthSummary.Name = "lblMonthSummary";
             lblMonthSummary.Size = new Size(75, 15);
             lblMonthSummary.TabIndex = 0;
@@ -365,9 +365,9 @@ namespace ScheduleProject
             panelList.BorderStyle = BorderStyle.FixedSingle;
             panelList.Controls.Add(dgvBudgets);
             panelList.Controls.Add(lblListTitle);
-            panelList.Location = new Point(40, 410);
+            panelList.Location = new Point(32, 360);
             panelList.Name = "panelList";
-            panelList.Size = new Size(820, 260);
+            panelList.Size = new Size(694, 196);
             panelList.TabIndex = 6;
             // 
             // dgvBudgets
@@ -380,14 +380,14 @@ namespace ScheduleProject
             dgvBudgets.ColumnHeadersHeight = 32;
             dgvBudgets.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
             dgvBudgets.GridColor = Color.FromArgb(226, 232, 240);
-            dgvBudgets.Location = new Point(28, 56);
+            dgvBudgets.Location = new Point(24, 50);
             dgvBudgets.MultiSelect = false;
             dgvBudgets.Name = "dgvBudgets";
             dgvBudgets.ReadOnly = true;
             dgvBudgets.RowHeadersVisible = false;
             dgvBudgets.RowTemplate.Height = 34;
             dgvBudgets.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            dgvBudgets.Size = new Size(764, 176);
+            dgvBudgets.Size = new Size(646, 124);
             dgvBudgets.TabIndex = 1;
             // 
             // lblListTitle
@@ -395,7 +395,7 @@ namespace ScheduleProject
             lblListTitle.AutoSize = true;
             lblListTitle.Font = new Font("맑은 고딕", 12F, FontStyle.Bold, GraphicsUnit.Point, 129);
             lblListTitle.ForeColor = Color.FromArgb(30, 41, 59);
-            lblListTitle.Location = new Point(26, 20);
+            lblListTitle.Location = new Point(24, 17);
             lblListTitle.Name = "lblListTitle";
             lblListTitle.Size = new Size(141, 21);
             lblListTitle.TabIndex = 0;
@@ -408,7 +408,7 @@ namespace ScheduleProject
             buttonRefresh.FlatStyle = FlatStyle.Flat;
             buttonRefresh.Font = new Font("맑은 고딕", 11F, FontStyle.Bold, GraphicsUnit.Point, 129);
             buttonRefresh.ForeColor = Color.White;
-            buttonRefresh.Location = new Point(624, 692);
+            buttonRefresh.Location = new Point(502, 575);
             buttonRefresh.Name = "buttonRefresh";
             buttonRefresh.Size = new Size(126, 40);
             buttonRefresh.TabIndex = 7;
@@ -424,7 +424,7 @@ namespace ScheduleProject
             buttonClose.FlatStyle = FlatStyle.Flat;
             buttonClose.Font = new Font("맑은 고딕", 11F, FontStyle.Bold, GraphicsUnit.Point, 129);
             buttonClose.ForeColor = Color.FromArgb(51, 65, 85);
-            buttonClose.Location = new Point(765, 692);
+            buttonClose.Location = new Point(631, 575);
             buttonClose.Name = "buttonClose";
             buttonClose.Size = new Size(95, 40);
             buttonClose.TabIndex = 8;
@@ -437,7 +437,7 @@ namespace ScheduleProject
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(248, 250, 252);
-            ClientSize = new Size(900, 760);
+            ClientSize = new Size(760, 635);
             Controls.Add(buttonClose);
             Controls.Add(buttonRefresh);
             Controls.Add(panelList);
