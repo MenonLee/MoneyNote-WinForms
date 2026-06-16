122906a5df3f9ae901e77c7cabbe9756c4ca15a7
MenonLee <rkong03@naver.com>
2026-06-16 23:40:48 +0900

    [Fix] Sync fixed expense from edit form

--- ScheduleProject/ScheduleProject/Forms/FormEditExpense.cs
+++ ScheduleProject/ScheduleProject/Forms/FormEditExpense.cs
@@ -192,6 +192,12 @@ namespace ScheduleProject.Forms
             expense.ExpenseDate = dateExpense.Value.Date;
             expense.Memo = textMemo.Text.Trim();
             expense.IsFixed = checkIsFixed.Checked;
+            expense.FixedExpenseRefId = checkIsFixed.Checked ? expense.FixedExpenseRefId : null;
+
+            if (expense.IsFixed)
+            {
+                expense.FixedExpenseRefId = DatabaseHelper.SaveFixedExpenseFromExpense(expense);
+            }
 
             DatabaseHelper.UpdateExpense(expense);
             MessageBox.Show("지출 내역을 수정했습니다.", "수정 완료", MessageBoxButtons.OK, MessageBoxIcon.Information);
